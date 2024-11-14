836a2a2aedddc5b1fbfc0f25b671cb5ed201a155
Clara Cerqueira <clarabcerq@gmail.com>
2024-11-14 11:04:01 -0300

    versão 2.0 - incluindo a classe Scanner para captação de dados do usuário

--- src/main/java/org/example/CalculoTaxaLiquida.java
+++ src/main/java/org/example/CalculoTaxaLiquida.java
@@ -1,17 +1,18 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class CalculoTaxaLiquida {
     public static void main(String[] args) {
         CalculoTaxaLiquida c = new CalculoTaxaLiquida();
-        c.capIn = 1000;
-        c.taxa_anual = 0.14;
-        c.anos = 2;
-        c.aliquota = 0.15;
+        c.lerEntradas();
+
         double montante = c.montante(c.capIn, c.taxa_anual, c.anos);
         double lb = c.lucroBruto();
         double ir = c.impostoRenda(c.aliquota);
         double mLiq = c.mLiquido();
         double taxaLiquida = c.taxaLiquidaAnual();
+
         System.out.println("O seu montante final bruto é R$" + String.format("%.2f", montante));
         System.out.println("Seu lucro bruto é de R$" + String.format("%.2f", lb));
         System.out.println("O imposto de renda que deverá ser pago é R$" + String.format("%.2f", ir));
@@ -19,27 +20,42 @@ public class CalculoTaxaLiquida {
         System.out.println("A sua taxa líquida anual, ou seja, quanto seu dinheiro renderá por ano é de " + String.format("%.2f", taxaLiquida) + "%");
     }
 
-    private double m;
     private double capIn;
     private double taxa_anual;
     private int anos;
     private double aliquota;
 
-    //1- Calcule o montante final bruto usando juros compostos
+    Scanner sc = new Scanner(System.in);
+
+    public void lerEntradas() {
+        System.out.println("Informe o capital inicial: ");
+        capIn = sc.nextDouble();
+
+        System.out.println("Informe a taxa anual (em decimal): ");
+        taxa_anual = sc.nextDouble();
+
+        System.out.println("Informe o número de anos: ");
+        anos = sc.nextInt();
+
+        System.out.println("Informe a aliquota (em decimal): ");
+        aliquota = sc.nextDouble();
+    }
+
+    //1- Montante final bruto usando juros compostos
     public double montante(double capIn, double taxa_anual, int anos) {
         double resultadoPotencia = Math.pow(1 + taxa_anual, anos);
         double m = capIn * resultadoPotencia;
         return m;
     }
 
-    //2- Calcule o lucro bruto subtraindo o capital inicial do montante final
+    //2- Lucro bruto
     public double lucroBruto(){
         double m = montante(capIn, taxa_anual, anos);
         double lucroBruto = m - capIn;
         return lucroBruto;
     }
 
-    //3- Calcule o IR. Como o investimento é de 2 anos, a alíquota de IR será de 15%.
+    //3- Imposto de Renda. Como o investimento é de 2 anos, a alíquota de IR será de 15%.
     public double impostoRenda(double aliquota) {
         double m = montante(capIn, taxa_anual, anos);
         double lucroBruto = m - capIn;
@@ -47,7 +63,7 @@ public class CalculoTaxaLiquida {
         return ir;
     }
 
-    //4- Encontre o montante líquido após o IR
+    //4- Montante líquido após o IR
     public double mLiquido() {
         double m = montante(capIn, taxa_anual, anos);
         double ir = impostoRenda(aliquota);
@@ -55,7 +71,7 @@ public class CalculoTaxaLiquida {
         return mLiq;
     }
 
-    //5- Calcule a taxa líquida anual
+    //5- Taxa líquida anual
     public double taxaLiquidaAnual() {
         double mLiq = mLiquido();
         double resultadoPotencia = Math.pow(mLiq / capIn, 1.0 / anos);
